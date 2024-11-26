6d37f4a2a5a165d8269b7f22caa8d9ce990e9214
Daniel <126785312+slhntg@users.noreply.github.com>
2024-11-26 23:09:45 +0000

    Commit 12

--- Movie.java
+++ Movie.java
@@ -1,26 +1,7 @@
 public class Movie {
 
     public double getCharge(int daysRented, Rental each){
-        double thisAmount = 0;
-
-             switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                   thisAmount += 2;
-                   if (each.getDaysRented() > 2) {
-                      thisAmount += (each.getDaysRented() - 2) * 1.5;
-                   }
-                   break;
-                case Movie.NEW_RELEASE:
-                   thisAmount += each.getDaysRented() * 3;
-                   break;
-                case Movie.CHILDRENS:
-                   thisAmount += 1.5;
-                   if (each.getDaysRented() > 3) {
-                      thisAmount += (each.getDaysRented() - 3) * 1.5;
-                   }
-                   break;
-             }
-          return thisAmount;
+        return _price.getCharge(daysRented, each);
     }
 
     public int getFrequentRenterPoints(int daysRented, Rental each){
--- Price.java
+++ Price.java
@@ -1,3 +1,26 @@
 public abstract class Price {
    public abstract int getPriceCode();
+
+public double getCharge(int daysRented, Rental each) {
+  double thisAmount = 0;
+
+     switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+           thisAmount += 2;
+           if (each.getDaysRented() > 2) {
+              thisAmount += (each.getDaysRented() - 2) * 1.5;
+           }
+           break;
+        case Movie.NEW_RELEASE:
+           thisAmount += each.getDaysRented() * 3;
+           break;
+        case Movie.CHILDRENS:
+           thisAmount += 1.5;
+           if (each.getDaysRented() > 3) {
+              thisAmount += (each.getDaysRented() - 3) * 1.5;
+           }
+           break;
+     }
+  return thisAmount;
+}
 }
