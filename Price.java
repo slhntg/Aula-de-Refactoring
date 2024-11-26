013bda34acadbaf73663cda8c8d191ef0d1d4206
Daniel <126785312+slhntg@users.noreply.github.com>
2024-11-26 23:21:53 +0000

    Commit 15

--- Price.java
+++ Price.java
@@ -4,11 +4,6 @@ public abstract class Price {
 public abstract double getCharge(int daysRented);
 
   public int getFrequentRenterPoints(int daysRented, Rental each) {
-    int frequentRenterPoints = 0;
-    // add frequent renter points
-    frequentRenterPoints ++;
-    if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-       each.getDaysRented() > 1) frequentRenterPoints ++;
-    return frequentRenterPoints;
+    return 1;
    }
 }
