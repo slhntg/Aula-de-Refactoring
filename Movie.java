b7ab22370e7f65a35be88fef8da2a9557ec03281
Daniel <126785312+slhntg@users.noreply.github.com>
2024-11-26 22:56:53 +0000

    Commit 9

--- Movie.java
+++ Movie.java
@@ -1,5 +1,28 @@
 public class Movie {
 
+    public double getCharge(int daysRented, Rental each){
+        double thisAmount = 0;
+
+             switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                   thisAmount += 2;
+                   if (each.getDaysRented() > 2) {
+                      thisAmount += (each.getDaysRented() - 2) * 1.5;
+                   }
+                   break;
+                case Movie.NEW_RELEASE:
+                   thisAmount += each.getDaysRented() * 3;
+                   break;
+                case Movie.CHILDRENS:
+                   thisAmount += 1.5;
+                   if (each.getDaysRented() > 3) {
+                      thisAmount += (each.getDaysRented() - 3) * 1.5;
+                   }
+                   break;
+             }
+          return thisAmount;
+    }
+
   public static final int  CHILDRENS = 2;
   public static final int  REGULAR = 0;
   public static final int  NEW_RELEASE = 1;
--- Rental.java
+++ Rental.java
@@ -1,26 +1,7 @@
 public class Rental {
 
-   public double getCharge(Rental each) {
-         double thisAmount = 0;
-
-         switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-               thisAmount += 2;
-               if (each.getDaysRented() > 2) {
-                  thisAmount += (each.getDaysRented() - 2) * 1.5;
-               }
-               break;
-            case Movie.NEW_RELEASE:
-               thisAmount += each.getDaysRented() * 3;
-               break;
-            case Movie.CHILDRENS:
-               thisAmount += 1.5;
-               if (each.getDaysRented() > 3) {
-                  thisAmount += (each.getDaysRented() - 3) * 1.5;
-               }
-               break;
-         }
-      return thisAmount;
+   public double getCharge() {
+      return _movie.getCharge(_daysRented, this);
    }
 
    public int getFrequentRenterPoints() { 
